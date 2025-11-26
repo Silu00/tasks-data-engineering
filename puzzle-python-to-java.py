97b60ef7f97ff024dff5e1a67942ae31ae877f5c
Silu00 <106192137+Silu00@users.noreply.github.com>
2025-11-26 15:50:57 +0100

    Update puzzle-python-to-java.py
    I had to force Unix line endings (\n) to ensure that 'diff' passes on Windows and then I stored the target Java code in raw string variable.

--- puzzle-python-to-java.py
+++ puzzle-python-to-java.py
@@ -1,1 +1,12 @@
-
+import sys;sys.stdout.reconfigure(newline='\n')
+HubertSilukTest = r"""public class Solution {
+    public static void main(String[] args) {
+    String HubertSilukTest = %c%s%c;
+    System.out.print("import sys;sys.stdout.reconfigure(newline='\\n')\n");
+    System.out.print("HubertSilukTest = r\"\"\"");
+    System.out.print(HubertSilukTest);
+    System.out.print("\"\"\"\n");
+    System.out.print("print(HubertSilukTest %% (34, HubertSilukTest.replace('\\\\', '\\\\\\\\').replace(chr(10), \"\\\\n\").replace(chr(34), \"\\\\\\\"\"), 34), end=\"\")");
+    }
+}"""
+print(HubertSilukTest % (34, HubertSilukTest.replace('\\', '\\\\').replace(chr(10), "\\n").replace(chr(34), "\\\""), 34), end="")
